6533b1c06f74724f875c04be9a0dd3feda598ac6
ttnny <ttnny@users.noreply.github.com>
2020-07-29 11:13:53 -0700

    Upgrade to use data binding technique

--- AboutMe/app/src/main/java/dev/ttnny/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/dev/ttnny/aboutme/MainActivity.kt
@@ -8,26 +8,35 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import dev.ttnny.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Albert Einstein")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+            binding.nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ AboutMe/app/src/main/java/dev/ttnny/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package dev.ttnny.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
