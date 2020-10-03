e7c41f38150adb2535a82c5b36d0a52d4f6b5721
ttnny <ttnny@users.noreply.github.com>
2020-10-03 00:58:06 -0700

    Create adapter for RecyclerView

---
+++ Affirmations/app/src/main/java/ttnny/dev/android/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package ttnny.dev.android.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+import ttnny.dev.android.affirmations.R
+import ttnny.dev.android.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+
+}
