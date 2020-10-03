13ca007645537291a6533bc6ba2976d1937f95c7
ttnny <ttnny@users.noreply.github.com>
2020-10-03 01:31:52 -0700

    Let RecyclerView use new Adapter

--- Affirmations/app/src/main/java/ttnny/dev/android/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/ttnny/dev/android/affirmations/adapter/ItemAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.list_item.view.*
 import ttnny.dev.android.affirmations.R
 import ttnny.dev.android.affirmations.model.Affirmation
 
