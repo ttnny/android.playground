a075385ebde40248f004ac2ec2f4f2773edc435c
ttnny <ttnny@users.noreply.github.com>
2021-01-20 18:29:56 -0800

    Update tests

--- AndroidTestingStarter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ AndroidTestingStarter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -2,6 +2,10 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,6 +22,9 @@ class TasksViewModelTest {
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
 
     }
 
