2791dbe8b372db519f79d1df5f95a6a879bccfd4
ttnny <ttnny@users.noreply.github.com>
2021-02-18 14:15:06 -0800

    Add project to learn Retrofit2, API, JSON

---
+++ Activities/app/src/main/java/ttnny/dev/android/activities/FirstActivity.kt
@@ -0,0 +1,11 @@
+package ttnny.dev.android.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
---
+++ Activities/app/src/main/java/ttnny/dev/android/activities/SecondActivity.kt
@@ -0,0 +1,11 @@
+package ttnny.dev.android.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
