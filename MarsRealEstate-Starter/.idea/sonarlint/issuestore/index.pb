fafe60183a7dc9848a089401522d56228066cfab
ttnny <ttnny@users.noreply.github.com>
2021-02-18 21:09:50 -0800

    Update

---
+++ MarsRealEstate-Starter/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+w
+Gapp/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt,d\d\dda4ecc7c5ce744b17df917a68aefad1710ac7bd
+t
+Dapp/src/main/java/com/example/android/marsrealestate/MainActivity.kt,6\d\6d9c28a11d772c4e9d5b83dc3a63f39a16e900f2
+Å
+Qapp/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt,3\6\366e6456d81f664a5ff7f8a7ebfe394e29fb9e02
+Ç
+Rapp/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt,9\3\9392d976cece25d352843080b92836948f4d37c0
+Å
+Qapp/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt,c\6\c61f1b5442e06797063779e5896bc1d69ea2e600
--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,10 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -44,6 +48,14 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
     }
+
 }
