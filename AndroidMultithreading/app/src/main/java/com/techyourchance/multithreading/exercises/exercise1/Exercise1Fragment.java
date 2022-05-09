2e1173717b1dc85ea190f5ded7f570a8b3e8f0ff
~tony <ttnny@users.noreply.github.com>
2022-05-09 14:44:32 -0700

    Completes exercise 1.

--- AndroidMultithreading/app/src/main/java/com/techyourchance/multithreading/exercises/exercise1/Exercise1Fragment.java
+++ AndroidMultithreading/app/src/main/java/com/techyourchance/multithreading/exercises/exercise1/Exercise1Fragment.java
@@ -52,17 +52,19 @@ public class Exercise1Fragment extends BaseFragment {
     }
 
     private void countIterations() {
-        long startTimestamp = System.currentTimeMillis();
-        long endTimestamp = startTimestamp + ITERATIONS_COUNTER_DURATION_SEC * 1000;
+        new Thread(() -> {
+            long startTimestamp = System.currentTimeMillis();
+            long endTimestamp = startTimestamp + ITERATIONS_COUNTER_DURATION_SEC * 1000;
 
-        int iterationsCount = 0;
-        while (System.currentTimeMillis() <= endTimestamp) {
-            iterationsCount++;
-        }
+            int iterationsCount = 0;
+            while (System.currentTimeMillis() <= endTimestamp) {
+                iterationsCount++;
+            }
 
-        Log.d(
-                "Exercise1",
-                "iterations in " + ITERATIONS_COUNTER_DURATION_SEC + "seconds: " + iterationsCount
-        );
+            Log.d(
+                    "Exercise1",
+                    "iterations in " + ITERATIONS_COUNTER_DURATION_SEC + "seconds: " + iterationsCount
+            );
+        }).start();
     }
 }
